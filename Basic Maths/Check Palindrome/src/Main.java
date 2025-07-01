f9dc1b0d5ae63d688d03a5c5532721f96903b91e
H Jivan Singha <hjivansingha123@gmail.com>
2025-07-01 19:33:46 +0530

    Palindrome

---
+++ Basic Maths/Check Palindrome/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean b=checkPalindrome(787);
+        System.out.println(b);
+    }
+    public static boolean checkPalindrome(int n){
+        int revSum=0;
+        int temp =n;
+    while(n!=0){
+        int lastDigit=n%10;
+        revSum=revSum*10 + lastDigit;
+        n=n/10;
+    }
+    if(temp<0){
+       revSum=revSum+'-';
+    }
+    return revSum==temp;
+    }
+}
