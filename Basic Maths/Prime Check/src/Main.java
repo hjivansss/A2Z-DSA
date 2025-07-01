b1d260701af35dbd01d5de9354696a062adbd919
H Jivan Singha <hjivansingha123@gmail.com>
2025-07-02 04:14:34 +0530

    Prime Check

---
+++ Basic Maths/Prime Check/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkPrime(12));
+    }
+
+    public static boolean checkPrime(int n){
+        int count =0;
+        for (int i = 1; i*i<= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (n / i != i) {
+                    count++;
+                }
+            }
+        }
+        if(count==2){
+            return true;
+        }else return false;
+    }
+}
