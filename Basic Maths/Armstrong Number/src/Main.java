5f55b97976d761a5c465e0e4e7860317c94064f4
H Jivan Singha <hjivansingha123@gmail.com>
2025-07-02 01:26:02 +0530

    Armstrong check

---
+++ Basic Maths/Armstrong Number/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+         System.out.println(armstrong(1634));
+
+    }
+
+    public static boolean armstrong(int n){
+        int digit=0;
+        double sum=0;
+        int temp=n;
+        int temp2=n;
+        while(temp!=0){
+            temp=temp/10;
+            digit++;
+        }
+
+        while(n>0){
+            int ld=n%10;
+            sum=sum+Math.pow(ld,digit);
+            n=n/10;
+        }
+
+        return sum==temp2;
+    }
+}
