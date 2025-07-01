03127ab2390433d1e7d573b01d4312df1832252d
H Jivan Singha <hjivansingha123@gmail.com>
2025-07-01 16:52:35 +0530

    Count Digits

---
+++ Basic Maths/Count Digits/Main.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args  ){
+        int count = countDigits(969857657);
+        System.out.println(count);
+    }
+    public static int countDigits(int n){
+        //edge case,if n=1, its one digit number
+        if(n==0){
+            return 1;
+        }
+
+        int count=0;
+        while(n!=0){
+            n=n/10;
+            count ++;
+        }
+       return count;
+
+    }
+}
